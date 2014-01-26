0ee84a09e6f550506ab7b1ddbd6d866c26c9e1f3
Gareth Rylance <gareth.rylance@aeriandi.com>
2014-01-26 21:04:20 +0000

    Add Grunt based build.

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+var files = [
+    'src/LayoutEngine.js',
+    'src/Render.js'
+];
+
+var banner = '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n';
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                    banner: banner,
+                    sourceMap:true,
+                    compress:true
+                },
+                build: {
+                    src: files,
+                    dest: 'build/Sequence.min.js'
+                }
+            }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
---
+++ src/LayoutEngine.js
@@ -0,0 +1,5 @@
+/**
+ * Created by garethr on 26/01/14.
+ */
+
+var engine ={};
